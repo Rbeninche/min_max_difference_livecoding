2896f5e7009c2a7642eadf6db8bccb6b013b9452
Reginald Beninche <rbeninche@gmail.com>
2019-10-29 22:17:22 -0400

    last version

--- array_difference.py
+++ array_difference.py
@@ -16,5 +16,22 @@ def min_max_diff(array):
     return max_number - min_number
 
 
+def returnMinMaxDiff(array):
+
+    array_sorted = sorted(array)
+
+    min = array_sorted[0]
+
+    max = array_sorted[-1]
+
+    print(min)
+    print(max)
+
+    return max - min
+
+
 if __name__ == '__main__':
     print(min_max_diff([15, 22, 84, 14, 88, 23]))
+
+    print("-" * 6)
+    print(returnMinMaxDiff([15, 22, 84, 14, 88, 23]))
